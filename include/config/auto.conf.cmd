858bc2ba928efb6a1335625d1aa99458482fd842
kylon <kylonhck@gmail.com>
2013-02-15 16:08:14 +0100

    2.6.32.43

--- include/config/auto.conf.cmd
+++ include/config/auto.conf.cmd
@@ -556,7 +556,7 @@ deps_config := \
 include/config/auto.conf: \
 	$(deps_config)
 
-ifneq "$(KERNELVERSION)" "2.6.32.42"
+ifneq "$(KERNELVERSION)" "2.6.32.43"
 include/config/auto.conf: FORCE
 endif
 ifneq "$(ARCH)" "arm"
--- include/linux/utsrelease.h
+++ include/linux/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.32.42-g4e79080-dirty"
+#define UTS_RELEASE "2.6.32.43-g18302d0-dirty"
